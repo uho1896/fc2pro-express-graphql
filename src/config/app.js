71f7a49f3894a2dd6af9f4806c4d4bfe41e599bc
uho1896 <uho1896@outlook.com>
2019-12-13 02:18:32 +0000

    read more configs from env

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,7 @@
 module.exports = {
-  port: 3000,
-  mailHost: 'smtp.office365.com',
-  mailPort: 587,
+  port: process.env.fc2proPort || 3000,
+  mailHost: process.env.fc2proMailHost || 'smtp.office365.com',
+  mailPort: process.env.fc2proMailPort || 587,
   mailUser: process.env.smtpMailUser,
   mailPassword: process.env.smtpMailPassword,
   jwtSecret: process.env.jwtSecret,
@@ -9,6 +9,6 @@ module.exports = {
   dbPassword: process.env.dbPassword,
   dbHost: process.env.dbHost,
   dbPort: process.env.dbPort,
-  activateBaseUri: 'http://localhost/user/activate',
-  resetBaseUri: 'http://localhost/user/resetPwd',
+  activateBaseUri: process.env.fc2proActiveUri || 'http://localhost/user/activate',
+  resetBaseUri: process.env.fc2proResetUri || 'http://localhost/user/resetPwd',
 };
